0b56b830f1bc37b7e2b88526b66ba910dcce65e5
Frank <fr.heggeness@gmail.com>
2019-04-19 09:39:48 -1000

    done and fixed test

--- largestProduct.js
+++ largestProduct.js
@@ -1,6 +1,40 @@
 class LargestProduct {
   constructor(number) {
+    this.getLargestProduct = function() {
+      let product = 0;
+      let arr = number
+        .toString(10)
+        .split('')
+        .map(Number);
 
+      for (let i = 0; i < arr.length; i++) {
+        let newProduct = arr[i] * arr[i + 1] * arr[i + 2] * arr[i + 3] * arr[i + 4];
+        if (newProduct > product) {
+          product = newProduct;
+        }
+      }
+
+      return product;
+    };
+
+    this.getLargestProductPosition = function() {
+      let product = 0;
+      let index = 0;
+      let arr = number
+        .toString(10)
+        .split('')
+        .map(Number);
+
+      for (let i = 0; i < arr.length; i++) {
+        let newProduct = arr[i] * arr[i + 1] * arr[i + 2] * arr[i + 3] * arr[i + 4];
+        if (newProduct >= product) {
+          product = newProduct;
+          index = i;
+        }
+      }
+
+      return index;
+    };
   }
 }
 
